837a5a7b26326c6a3aaccc59dd805c37d1722ea2
Ivaz <ivazreza@gmail.com>
2025-05-05 16:22:43 +0700

    fix: loading log out design

--- src/components/ui/dashboard/logOutBtn.tsx
+++ src/components/ui/dashboard/logOutBtn.tsx
@@ -8,6 +8,7 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
+import { RotatingLines } from 'react-loader-spinner'
 
 export default function LogOutBtn () {
   const logOutMutation = useLogOutMutation();
@@ -19,9 +20,10 @@ export default function LogOutBtn () {
   return (
     <>
       <AlertDialog open={logOutMutation.isPending}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Logging out...</AlertDialogTitle>
+        <AlertDialogContent className="flex justify-center w-[350px] h-[200px] items-center">
+          <AlertDialogHeader className="flex flex-col items-center gap-2">
+            <AlertDialogTitle>Logging out</AlertDialogTitle>
+            <RotatingLines width="40" strokeColor="#fff" />
           </AlertDialogHeader>
         </AlertDialogContent>
       </AlertDialog>
