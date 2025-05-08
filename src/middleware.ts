5a99358910571b4509f7a16e5b98c8524915480f
Ivaz <ivazreza@gmail.com>
2025-05-08 15:45:36 +0700

    chore: middleware added to api route request

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,5 +19,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/dashboard/:path*', '/login', '/register'],
+  matcher: ['/', '/dashboard/:path*', '/login', '/register', '/api/logout', '/api/todos/:path*'],
 }
