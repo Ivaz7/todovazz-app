6441313e57596b10cd72ce96e7757414f3314cb3
Ivaz <ivazreza@gmail.com>
2025-05-08 17:08:27 +0700

    feat: adding a link source code to the footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Footer () {
   return (
     <footer className="flex p-4 justify-center items-center border-t border-neutral-700">
       <p>
-        Test Footer
+        This App Created By Ivaz Reza. Code source available at my <Link className="hover:underline text-neutral-500" target="_blank" href="https://github.com/Ivaz7/todovazz-app">GitHub</Link>
       </p>
     </footer>
   )
